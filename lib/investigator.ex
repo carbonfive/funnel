6aeb3d318e44c888151eedf4d84b342469f4159b
Evelyn Masso <evelyn@carbonfive.com>
2017-09-19 15:04:02 -0700

    clean up investigator

--- lib/investigator.ex
+++ lib/investigator.ex
@@ -1,13 +1,5 @@
 defmodule Funnel.Investigator do
-
-  def get_scent(body) do
-    %{
-      "owner_login" => body["repository"]["owner"]["login"],
-      "repo_name" => body["repository"]["name"],
-      "commit_sha" => body["head_commit"]["id"],
-      "default_branch_name" => body["repository"]["default_branch"]
-    }
-  end
+  @tenta_client Tentacat.Client.new(%{access_token: "15f30976459d7dd25b5f1366b3881b2d07b32c53"})
 
   def investigate(body) do
     scent = get_scent(body)
@@ -19,48 +11,53 @@ defmodule Funnel.Investigator do
     end
   end
 
-  def is_default_push(body) do
+  defp get_scent(body) do
+    %{
+      "owner_login" => body["repository"]["owner"]["login"],
+      "repo_name" => body["repository"]["name"],
+      "commit_sha" => body["head_commit"]["id"],
+      "default_branch_name" => body["repository"]["default_branch"]
+    }
+  end
+
+  defp is_default_push(body) do
     String.ends_with? body["ref"], body["repository"]["default_branch"]
   end
 
-  # should fail all branches?
-  def fail_all_branches(scent) do
+  # should fail all branches
+  defp fail_all_branches(scent) do
     # get all branches in repo
-    branches_res = Tentacat.Repositories.Branches.list scent["owner_login"], scent["repo_name"], tenta_client()
+    branches_res = Tentacat.Repositories.Branches.list scent["owner_login"], scent["repo_name"], @tenta_client
     Enum.each branches_res, fn(b) ->
       if b["name"] !== scent["default_branch"] do
         spawn fn ->
-          Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], b["commit"]["sha"], failure_status_body(), tenta_client()
+          Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], b["commit"]["sha"], failure_status_body(), @tenta_client
         end
       end
     end
   end
 
-  def investigate_push(scent) do
+  defp investigate_push(scent) do
     # mark as pending
-    Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], scent["commit_sha"], pending_status_body(), tenta_client()
+    Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], scent["commit_sha"], pending_status_body(), @tenta_client
 
     # get commit branch head
-    commit_parent_sha = hd(Tentacat.Commits.find(scent["commit_sha"], scent["owner_login"], scent["repo_name"], tenta_client())["parents"])["sha"]
+    commit_parent_sha = hd(Tentacat.Commits.find(scent["commit_sha"], scent["owner_login"], scent["repo_name"], @tenta_client)["parents"])["sha"]
 
     # get default branch head
-    branch_sha = Tentacat.Repositories.Branches.find(scent["owner_login"], scent["repo_name"], scent["default_branch_name"], tenta_client())["commit"]["sha"]
+    branch_sha = Tentacat.Repositories.Branches.find(scent["owner_login"], scent["repo_name"], scent["default_branch_name"], @tenta_client)["commit"]["sha"]
 
     # compare and update status
-    if commit_parent_sha === branch_sha do
-      chosen_status_body = success_status_body()
-    else
-      chosen_status_body = failure_status_body()
-    end
+    chosen_status_body =
+      case commit_parent_sha === branch_sha do
+        true -> success_status_body()
+        false -> failure_status_body()
+      end
 
-    Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], scent["commit_sha"], chosen_status_body, tenta_client()
+    Tentacat.Repositories.Statuses.create scent["owner_login"], scent["repo_name"], scent["commit_sha"], chosen_status_body, @tenta_client
   end
 
-  def tenta_client() do
-    Tentacat.Client.new(%{access_token: "15f30976459d7dd25b5f1366b3881b2d07b32c53"})
-  end
-
-  def pending_status_body() do
+  defp pending_status_body() do
     %{
        "state": "pending",
        "description": "Investigating your commit",
@@ -68,7 +65,7 @@ defmodule Funnel.Investigator do
      }
   end
 
-  def success_status_body() do
+  defp success_status_body() do
     %{
        "state": "success",
        "description": "Commit is good to merge",
@@ -76,7 +73,7 @@ defmodule Funnel.Investigator do
      }
   end
 
-  def failure_status_body() do
+  defp failure_status_body() do
     %{
        "state": "failure",
        "description": "Commit must be rebased and squashed",
