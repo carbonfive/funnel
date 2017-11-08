87903eb3cb021779731122d54d76a2c0c1dc7d11
Evelyn Masso <evelyn@carbonfive.com>
2017-11-08 14:03:02 -0800

    temporary dep location change

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Funnel.Mixfile do
     [
       {:cowboy, "~> 1.0"},
       {:plug, "~> 1.0"},
-      {:tentacat, "~> 0.7"},
+      # {:tentacat, "~> 0.7"},
+      {:tentacat, github: "outofambit/tentacat", branch: "installation-api"},
       {:poison, "~> 3.0"},
       {:ex_machina, "~> 2.1", only: :test},
       {:mock, "~> 0.3", only: :test},
