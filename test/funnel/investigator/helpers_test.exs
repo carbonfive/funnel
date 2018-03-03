fec835d4dca21035c6afd4f02b0aeec1fcbf801a
Evelyn Masso <evelyn@carbonfive.com>
2018-03-02 16:41:28 -0800

    add (more) tests for investigator helpers

--- test/funnel/investigator/helpers_test.exs
+++ test/funnel/investigator/helpers_test.exs
@@ -10,7 +10,7 @@ defmodule Funnel.Investigator.HelpersTest do
     HTTPoison.start
   end
 
-  test "branch pushed fails associated pull requests statuses" do
+  test "fail_open_pull_requests/2 fails all associated open pull requests" do
     with_mocks([
       {Tentacat.Repositories.Statuses, [:passthrough], []}
     ]) do
@@ -39,6 +39,47 @@ defmodule Funnel.Investigator.HelpersTest do
           :_
         )
       end
+    end
+  end
+
+  test "mark_commit_pending/2 sends a single pending status" do
+    with_mocks([
+      {Tentacat.Repositories.Statuses, [:passthrough], []}
+    ]) do
+      use_cassette "mark_commit_pending" do
+        Funnel.Investigator.Helpers.mark_commit_pending build(:push_scent), @tenta_client
+
+        assert called Tentacat.Repositories.Statuses.create(
+          build(:push_scent).owner_login,
+          build(:push_scent).repo_name,
+          build(:push_scent).commit_sha,
+          build(:pending_status),
+          :_
+        )
+        refute called Tentacat.Repositories.Statuses.create(
+          build(:push_scent).owner_login,
+          build(:push_scent).repo_name,
+          :_,
+          build(:failure_status),
+          :_
+        )
+        refute called Tentacat.Repositories.Statuses.create(
+          build(:push_scent).owner_login,
+          build(:push_scent).repo_name,
+          :_,
+          build(:success_status),
+          :_
+        )
+      end
+    end
+  end
+
+  test "get_base_sha/2 returns a sha" do
+    use_cassette "default_branch_pushed" do
+      res = Funnel.Investigator.Helpers.get_base_sha build(:push_scent), @tenta_client
+
+      assert is_binary(res)
+      refute is_list(res)
     end
   end
 
