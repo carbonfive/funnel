be3cbf1fde087d350cdf37471adcf668b29cde00
Evelyn Masso <evelyn@carbonfive.com>
2018-03-27 16:50:19 -0700

    use module attr for nil strategy name

--- lib/funnel_web/views/repositories_view.ex
+++ lib/funnel_web/views/repositories_view.ex
@@ -3,6 +3,8 @@ defmodule FunnelWeb.RepositoriesView do
   alias Funnel.GitHub.Repository
   alias Funnel.Git
 
+  @nil_strategy_name "None"
+
   @spec repository_name(%Repository{}) :: binary
   def repository_name(repository) do
     repository.details.owner <> "/" <> repository.details.name
@@ -12,7 +14,7 @@ defmodule FunnelWeb.RepositoriesView do
   def repository_strategy_name(repository) do
     case repository.strategy do
       nil ->
-        "None"
+        @nil_strategy_name
       strategy ->
         strategy.name
     end
@@ -21,7 +23,7 @@ defmodule FunnelWeb.RepositoriesView do
   @spec repository_strategy_name(list(%Git.Strategy{})) :: list({})
   def repository_strategy_options(strategies) do
     Enum.map(strategies, &{&1.name, &1.id})
-    |> List.insert_at(0, {"None", ""})
+    |> List.insert_at(0, {@nil_strategy_name, ""})
   end
 
 end
